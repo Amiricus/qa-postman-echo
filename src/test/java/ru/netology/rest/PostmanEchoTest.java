6013e2427efc98d0986d193ed92f9b64ce150529
Амир Байтулинов <amiricus@gmail.com>
2022-08-06 19:30:32 +0500

    Second commit

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -7,7 +7,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 class PostmanEchoTest {
     @Test
-    void shouldPostmanEcho() {
+    void shouldPostmanEchoTrue() {
         given()
                 .baseUri("https://postman-echo.com")
                 .body("some data")
@@ -16,6 +16,19 @@ class PostmanEchoTest {
                 .then()
                 .statusCode(200)
                 .body("data", equalTo("some data"))
+        ;
+    }
+
+    @Test
+    void shouldPostmanEchoFalse() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("My name is Amir")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello"))
         ;
     }
 }
